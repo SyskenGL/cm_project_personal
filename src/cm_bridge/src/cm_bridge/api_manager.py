7989aef60326938d3569f24bae5e1c38342b53bc
SyskenGL <gianluca.larco@gmail.com>
2021-10-27 22:46:58 +0200

    Initial

--- src/cm_bridge/src/cm_bridge/api_manager.py
+++ src/cm_bridge/src/cm_bridge/api_manager.py
@@ -2,7 +2,10 @@
 import rospy
 import serial
 import actionlib
+from std_msgs.msg import String
+from collections import defaultdict
 from cm_ros.wrapper import CMNode
+from cm_msgs.msg import CallAPIAction, CallAPIResult
 
 
 class CMAPIManager(CMNode):
@@ -10,24 +13,44 @@ class CMAPIManager(CMNode):
     def __init__(self, name='cm_api_manager'):
         CMNode.__init__(self, name)
 
-        # Serial port used to communicate with the robot
-        self.serial_port = rospy.get_param('~serial_port')
+        # Set initial configurations
+        self.config = defaultdict(lambda: None)
+        self.__init_config()
 
         # Serial Stream
         self.__serial = None
-        self.subscribe()
+        self.__subscribe()
 
-    def subscribe(self):
+        # ROS Topic used to publish serial output
+        self.__pub_serial = rospy.Publisher('~serial', String, queue_size=10)
+
+    def __init_config(self):
+        # Serial port used to communicate with the robot
+        self.config['serial_port'] = rospy.get_param('~serial_port')
+
+        # Number of symbols transmitted in one second
+        self.config['baud_rate'] = rospy.get_param('~baud_rate')
+
+    def __subscribe(self):
         try:
-            self.__serial = serial.Serial(self.serial_port)
+            self.__serial = serial.Serial(
+                self.config['serial_port'],
+                self.config['baud_rate']
+            )
         except serial.SerialException as err:
             rospy.logerr('[{name}] - {err}'.format(name=rospy.get_name(), err=err))
             raise err
 
-    def unsubscribe(self):
+    def __unsubscribe(self):
         if (self.__serial is not None) and self.__serial.isOpen():
             self.__serial.close()
 
+    def __listen(self):
+        # Listen on the serial port then publish on the topic
+        while not rospy.is_shutdown():
+            serial_out = self.__serial.readline()
+            self.__pub_serial.publish(serial_out)
+
     def run(self):
-        while True:
-            pass
+        self.__listen()
+
