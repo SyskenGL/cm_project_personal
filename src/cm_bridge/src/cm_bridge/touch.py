de2e213e948f4b830355766773864f1403476569
SyskenGL <gianluca.larco@gmail.com>
2021-11-01 19:10:22 +0100

    Init

--- src/cm_bridge/src/cm_bridge/touch.py
+++ src/cm_bridge/src/cm_bridge/touch.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from cm_ros.wrapper import CMNode
+from cm_msgs.msg import SetTouchAction, TouchStatus
 
 
 class CMTouch(CMNode):
@@ -7,3 +8,5 @@ class CMTouch(CMNode):
     def __init__(self, name='cm_touch'):
         CMNode.__init__(self, name)
 
+    def run(self):
+        pass
