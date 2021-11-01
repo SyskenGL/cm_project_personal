d1c7c94ec51294e4e78a72620710cedac991c917
SyskenGL <gianluca.larco@gmail.com>
2021-11-01 17:47:42 +0100

    Initial

--- src/cm_bridge/src/cm_bridge/api_manager.py
+++ src/cm_bridge/src/cm_bridge/api_manager.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
-import re
 import rospy
 import serial
 import actionlib
 from std_msgs.msg import String
 from collections import defaultdict
 from cm_ros.wrapper import CMNode
-from cm_msgs.msg import CallAPIAction
+from cm_msgs.msg import CallAPIAction, CallAPIResult
 
 
 class CMAPIManager(CMNode):
@@ -25,6 +24,9 @@ class CMAPIManager(CMNode):
         # ROS Topic used to publish serial output
         self.__pub_serial = rospy.Publisher('~serial', String, queue_size=5)
 
+        # Unique ID of call request
+        self.__api_call_id = -1
+
         # Initialize Action Server used to call API
         self.__call_api_srv = actionlib.SimpleActionServer(
             rospy.get_name(),
@@ -40,18 +42,22 @@ class CMAPIManager(CMNode):
         # Number of symbols transmitted in one second
         self.config['baud_rate'] = rospy.get_param('~baud_rate')
 
-        # Maximum time that each call to readline will wait.
-        self.config['timeout'] = rospy.get_param('~timeout')
+        # Maximum time that each call to read will wait.
+        self.config['read_timeout'] = rospy.get_param('~read_timeout')
+
+        # Maximum time that each call to write will wait.
+        self.config['write_timeout'] = rospy.get_param('~write_timeout')
 
     def __subscribe(self):
         try:
             self.__serial = serial.Serial(
                 self.config['serial_port'],
                 self.config['baud_rate'],
-                timeout=self.config['timeout']
+                timeout=self.config['read_timeout'],
+                write_timeout=self.config['write_timeout']
             )
         except serial.SerialException as err:
-            rospy.logerr('[{name}] - {err}'.format(name=rospy.get_name(), err=err))
+            rospy.logerr('Node {name} - {err}'.format(name=rospy.get_name(), err=err))
             raise err
 
     def __unsubscribe(self):
@@ -62,11 +68,33 @@ class CMAPIManager(CMNode):
         # Listen on the serial port then publish on the topic
         while not rospy.is_shutdown():
             out = self.__serial.readline()
-            if out:
+            if out and not rospy.is_shutdown():
                 self.__pub_serial.publish(out)
 
+    def __get_api_call_id(self):
+        self.__api_call_id = (self.__api_call_id + 1) % 65536
+        return self.__api_call_id
+
     def __on_api_call_received(self, goal):
-        pass
+        succeed = True
+
+        # Complete the request by adding the call api ID
+        api_call_id = self.__get_api_call_id()
+        goal.request += '[{id}|\n'.format(id=api_call_id)
+
+        # Write on serial port
+        try:
+            self.__serial.write(bytes(goal.request, 'utf-8'))
+        except serial.SerialTimeoutException as err:
+            rospy.logwarn('Node {name} - {err}'.format(name=rospy.get_name(), err=err))
+            succeed = False
+
+        # Submit the result
+        result = CallAPIResult(id=api_call_id)
+        if succeed:
+            self.__call_api_srv.set_succeeded(result)
+        else:
+            self.__call_api_srv.set_aborted(result)
 
     def run(self):
         self.__call_api_srv.start()
