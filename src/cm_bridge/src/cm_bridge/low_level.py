78d093ba64fc9b8ef109ad52ca0cf058e443843b
SyskenGL <gianluca.larco@gmail.com>
2021-11-07 23:40:20 +0100

    Init

--- src/cm_bridge/src/cm_bridge/low_level.py
+++ src/cm_bridge/src/cm_bridge/low_level.py
@@ -7,83 +7,109 @@ from std_msgs.msg import String
 from collections import defaultdict
 from cm_ros.wrapper import CMNode
 from cm_msgs.msg import WriteOnSerialAction, WriteOnSerialResult
-from cm_msgs.msg import TouchInfo, EnableTouchGoal, EnableBarrierGoal, EnableSoundGoal
-
-
-MSG_INFO = {
-    'header': '#',
-    'flags': {
-        'status': 'R',
-        'motors': 'M',
-        'leds': 'L',
-        'sound': 'S',
-        'ir': 'O',
-        'touch': 'T',
-        'display': 'V'
-    },
-    'flags_sep': '|',
-    'values_sep': ',',
-    'footer': '[{id}|\n'
-}
-REQUEST_STRUCT = '{header}{flag}:{content}{footer}'
+from cm_msgs.msg import TouchInfo, DisplayInfo, IRInfo, SoundInfo, MotorsInfo, LEDsInfo
 
 
 def compose_status_request():
-    return REQUEST_STRUCT.format(
-        header=MSG_INFO['header'],
-        flag=MSG_INFO['flags']['status'],
-        content='',
-        footer=MSG_INFO['footer']
-    )
+    return '#R:[{id}|\n'
 
 
-def compose_touch_request(enable_touch):
-    content = '{touch_enabled}'.format(touch_enabled=int(enable_touch.touch_enabled))
-    return REQUEST_STRUCT.format(
-        header=MSG_INFO['header'],
-        flag=MSG_INFO['flags']['touch'],
-        content=content,
-        footer=MSG_INFO['footer']
-    )
+def unpack_motors_info(msg):
+    motors_info = None
+    regex_result = re.search('M:(.+?)\|', msg)
+    if regex_result:
+        values = [int(value) for value in regex_result.group(1).split(',')]
+        motors_info = MotorsInfo(
+            m0_angle=values[0],
+            m1_angle=values[1],
+            m2_angle=values[2],
+            m3_angle=values[3],
+            m4_angle=values[4],
+            m5_angle=values[5]
+        )
+    return motors_info
 
 
-def compose_ir_request(enable_barrier):
-    content = '{barrier_enabled}'.format(barrier_enabled=int(enable_barrier.barrier_enabled))
-    return REQUEST_STRUCT.format(
-        header=MSG_INFO['header'],
-        flag=MSG_INFO['flags']['ir'],
-        content=content,
-        footer=MSG_INFO['footer']
-    )
+def unpack_leds_info(msg):
+    leds_info = None
+    regex_result = re.search('L:(.+?)\|', msg)
+    if regex_result:
+        values = [int(value) for value in regex_result.group(1).split(',')]
+        leds_info = LEDsInfo(
+            front_led_enabled=bool(values[0]),
+            front_led_intensity=values[1],
+            left_led_enabled=bool(values[2]),
+            left_led_red=values[3],
+            left_led_green=values[4],
+            left_led_blue=values[5],
+            left_led_blink=bool(values[6]),
+            right_led_enabled=bool(values[7]),
+            right_led_red=values[8],
+            right_led_green=values[9],
+            right_led_blue=values[10],
+            right_led_blink=bool(values[11])
+        )
+    return leds_info
 
 
-def compose_sound_request(enable_sound):
-    content = '{sound_enabled}' + MSG_INFO['values_sep'] + '{auto_follow_enabled}'
-    content = content.format(
-        sound_enabled=int(enable_sound.sound_enabled),
-        auto_follow_enabled=int(enable_sound.auto_follow_enabled)
-    )
-    return REQUEST_STRUCT.format(
-        header=MSG_INFO['header'],
-        flag=MSG_INFO['flags']['sound'],
-        content=content,
-        footer=MSG_INFO['footer']
-    )
+def unpack_sound_info(msg):
+    sound_info = None
+    regex_result = re.search('S:(.+?)\|', msg)
+    if regex_result:
+        values = [int(value) for value in regex_result.group(1).split(',')]
+        sound_info = SoundInfo(
+            enabled=bool(values[0]),
+            auto_follow=bool(values[1]),
+            front=bool(values[2]),
+            rear=bool(values[3]),
+            right=bool(values[4]),
+            left=bool(values[5]),
+            front_right=bool(values[6]),
+            front_left=bool(values[7]),
+            rear_right=bool(values[8]),
+            rear_left=bool(values[9])
+        )
+    return sound_info
+
+
+def unpack_ir_info(msg):
+    ir_info = None
+    regex_result = re.search('O:(.+?)\|', msg)
+    if regex_result:
+        values = [int(value) for value in regex_result.group(1).split(',')]
+        ir_info = IRInfo(
+            enabled=bool(values[0]),
+            front_distance=values[1],
+            rear_distance=values[2],
+            right_distance=values[3],
+            left_distance=values[4],
+            violation=bool(values[5])
+        )
+    return ir_info
 
 
 def unpack_touch_info(msg):
     touch_info = None
-    regex = MSG_INFO['flags']['touch'] + '(.+?)\\' + MSG_INFO['flags_sep']
-    regex_result = re.search(regex, msg)
+    regex_result = re.search('T:(.+?)\|', msg)
     if regex_result:
-        values = [int(value) for value in regex_result.group(1).split(MSG_INFO['values_sep'])]
+        values = [int(value) for value in regex_result.group(1).split(',')]
         touch_info = TouchInfo(
-            touch_enabled=bool(values[0]),
-            lt_sensor=bool(values[1]),
+            enabled=bool(values[0]),
+            left_sensor=bool(values[1]),
             up_sensor=bool(values[2]),
-            rt_sensor=bool(values[3])
+            right_sensor=bool(values[3])
         )
     return touch_info
+
+
+def unpack_display_info(msg):
+    display_info = None
+    msg = re.sub('\[(.+?)\|', '|', msg)
+    regex_result = re.search('V:(.+?)\|', msg)
+    if regex_result:
+        values = [int(value) for value in regex_result.group(1).split(',')]
+        display_info = DisplayInfo(face_code=values[0])
+    return display_info
 
 
 class CMLowLevel(CMNode):
@@ -133,7 +159,7 @@ class CMLowLevel(CMNode):
     def __listen_serial(self):
         while not rospy.is_shutdown():
             if msg := str(self.__serial.readline(), 'utf-8'):
-                if msg[0] == MSG_INFO['header']:
+                if msg[0] == '#':
                     if re.search(r'\[\d+\|$', msg):
                         self.__pub_responses.publish(msg)
                     else:
