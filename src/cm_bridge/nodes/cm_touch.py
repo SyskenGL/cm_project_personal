136e5d0ad1ec3bd2693e7c7d278c39badf51279b
SyskenGL <gianluca.larco@gmail.com>
2021-11-07 21:39:11 +0100

    Init

---
+++ src/cm_bridge/nodes/cm_touch.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+from cm_bridge.touch import CMTouch
+
+
+if __name__ == '__main__':
+    cm_touch = CMTouch()
+    cm_touch.start()
--- src/cm_bridge/src/cm_bridge/low_level.py
+++ src/cm_bridge/src/cm_bridge/low_level.py
@@ -7,6 +7,60 @@ from std_msgs.msg import String
 from collections import defaultdict
 from cm_ros.wrapper import CMNode
 from cm_msgs.msg import WriteOnSerialAction, WriteOnSerialResult
+from cm_msgs.msg import TouchInfo
+
+
+MSG_INFO = {
+    'header': '#',
+    'body': {
+        'flags': {
+            'status': 'R',
+            'motors': 'M',
+            'leds': 'L',
+            'sound': 'S',
+            'ir': 'O',
+            'touch': 'T',
+            'display': 'V'
+        },
+        'flags_sep': '|',
+        'values_sep': ',',
+    },
+    'footer': '[{id}|\n'
+}
+
+
+def compose_status_request():
+    request = '{header}{flag}:{footer}'
+    return request.format(
+        header=MSG_INFO['header'],
+        flag=MSG_INFO['body']['flags']['status'],
+        footer=MSG_INFO['footer']
+    )
+
+
+def compose_touch_request(enabled):
+    request = '{header}{flag}:{enabled}{footer}'
+    return request.format(
+        header=MSG_INFO['header'],
+        flag=MSG_INFO['body']['flags']['status'],
+        enabled=int(enabled),
+        footer=MSG_INFO['footer']
+    )
+
+
+def unpack_touch_info(msg):
+    touch_info = None
+    regex = MSG_INFO['body']['flags']['touch'] + '(.+?)\\' + MSG_INFO['body']['flags_sep']
+    regex_result = re.search(regex, msg)
+    if regex_result:
+        values = [int(value) for value in regex_result.group(1).split(MSG_INFO['body']['values_sep'])]
+        touch_info = TouchInfo(
+            enable=bool(values[0]),
+            lt_sensor=bool(values[1]),
+            up_sensor=bool(values[2]),
+            rt_sensor=bool(values[3])
+        )
+    return touch_info
 
 
 class CMLowLevel(CMNode):
@@ -24,10 +78,10 @@ class CMLowLevel(CMNode):
         self.__pub_responses = rospy.Publisher('~responses', String, queue_size=5, latch=True)
 
         self.__write_on_serial_id = 0
-        self.__serial_as = actionlib.SimpleActionServer(
-            '~serial_as',
+        self.__serial_server = actionlib.SimpleActionServer(
+            '~serial_server',
             WriteOnSerialAction,
-            execute_cb=self.__on_serial_write_requested,
+            execute_cb=self.__on_serial_write,
             auto_start=False
         )
 
@@ -56,7 +110,7 @@ class CMLowLevel(CMNode):
     def __listen_serial(self):
         while not rospy.is_shutdown():
             if msg := str(self.__serial.readline(), 'utf-8'):
-                if msg[0] == '#':
+                if msg[0] == MSG_INFO['header']:
                     if re.search(r'\[\d+\|$', msg):
                         self.__pub_responses.publish(msg)
                     else:
@@ -65,7 +119,7 @@ class CMLowLevel(CMNode):
                     rospy.logdebug('Node {name} - serial: {msg}'.format(name=rospy.get_name(), msg=msg))
         self.__close_serial()
 
-    def __on_serial_write_requested(self, request):
+    def __on_serial_write(self, request):
         succeed = True
 
         self.__write_on_serial_id = (self.__write_on_serial_id % 65535) + 1
@@ -81,6 +135,16 @@ class CMLowLevel(CMNode):
         else:
             self.__call_api_server.set_aborted(result)
 
+    def __get_initial_status(self):
+        try:
+            self.__serial.write(
+                bytes(compose_status_request().format(id=self.__write_on_serial_id), 'utf-8')
+            )
+        except serial.SerialTimeoutException as err:
+            rospy.logerr('Node {name} - {err}'.format(name=rospy.get_name(), err=err))
+            raise err
+
     def run(self):
-        self.__serial_as.start()
+        self.__get_initial_status()
+        self.__serial_server.start()
         self.__listen_serial()
