ca2433c3480e234849bc91e8f2938ea5a7fc1dd8
SyskenGL <gianluca.larco@gmail.com>
2021-11-08 01:44:21 +0100

    Init

---
+++ src/cm_bridge/nodes/cm_microphone.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+from cm_bridge.microphone import CMMicrophone
+
+
+if __name__ == '__main__':
+    cm_microphone = CMMicrophone()
+    cm_microphone.start()
--- src/cm_bridge/src/cm_bridge/microphone.py
+++ src/cm_bridge/src/cm_bridge/microphone.py
@@ -12,58 +12,46 @@ class CMMicrophone(CMNode):
     def __init__(self, name='cm_microphone'):
         CMNode.__init__(self, name)
 
+        self.__config = defaultdict(lambda: None)
+        self.__configure()
+
         self.__audio_stream = None
+        self.__open_audio_stream()
 
         self.__pub_audio = rospy.Publisher('~audio', Audio, queue_size=5)
-        self.pub_audio_info = rospy.Publisher('~audio_info', AudioInfo, queue_size=10, latch=True)
+        self.__pub_audio_info = rospy.Publisher('~audio_info', AudioInfo, queue_size=10, latch=True)
 
-        # Set initial configurations
-        self.config = defaultdict(lambda: None)
-        self.init_config()
-
-    def init_config(self):
-        # Mandatory configurations to be set
-        self.config['chunk_exp'] = rospy.get_param('~chunk_exp')
-        self.config['rate'] = rospy.get_param('~rate')
-        self.config['channels'] = rospy.get_param('~channels')
-        self.config['format'] = rospy.get_param('~format')
-
-        # Apply initial configurations
-        self.subscribe()
-        rospy.loginfo('{name} Initial configurations: {config}.'.format(
-            name=rospy.get_name(),
-            config=self.config
+        self.__pub_audio_info.publish(AudioInfo(
+            chunk=pow(2, self.__config['chunk_exp']),
+            rate=self.__config['rate'],
+            channels=self.__config['channels'],
+            format=self.__config['format']
         ))
 
-        # Publish Audio Info
-        audio_info_msg = AudioInfo(
-            chunk=pow(2, self.config['chunk_exp']),
-            rate=self.config['rate'],
-            channels=self.config['channels'],
-            format=self.config['format']
-        )
-        self.pub_audio_info.publish(audio_info_msg)
+    def __configure(self):
+        self.__config['chunk_exp'] = rospy.get_param('~chunk_exp')
+        self.__config['rate'] = rospy.get_param('~rate')
+        self.__config['channels'] = rospy.get_param('~channels')
+        self.__config['format'] = rospy.get_param('~format')
 
-    def subscribe(self):
-        # Create an Audio Stream
-        self.audio_stream = pyaudio.PyAudio().open(
-            format=self.config['format'],
-            channels=self.config['channels'],
-            rate=self.config['rate'],
-            frames_per_buffer=pow(2, self.config['chunk_exp']),
+    def __open_audio_stream(self):
+        self.__audio_stream = pyaudio.PyAudio().open(
+            format=self.__config['format'],
+            channels=self.__config['channels'],
+            rate=self.__config['rate'],
+            frames_per_buffer=pow(2, self.__config['chunk_exp']),
             input=True
         )
 
-    def unsubscribe(self):
-        if self.audio_stream is not None and self.audio_stream.is_active():
-            self.audio_stream.close()
+    def __close_audio_stream(self):
+        if self.__audio_stream is not None and self.__audio_stream.is_active():
+            self.__audio_stream.close()
 
     def run(self):
         while not rospy.is_shutdown():
-            # Create the Audio msg and public the Audio msg
             audio_msg = Audio(
                 header=Header(stamp=rospy.Time.now()),
-                data=self.audio_stream.read(pow(2, self.config['chunk_exp']))
+                data=self.__audio_stream.read(pow(2, self.__config['chunk_exp']))
             )
-            self.pub_audio.publish(audio_msg)
-        self.unsubscribe()
+            self.__pub_audio.publish(audio_msg)
+        self.__close_audio_stream()
